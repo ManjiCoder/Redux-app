9ad29246270db7b2a2d22fd74fc6763a4955a658
ManjiCoder <manjicoder@gmail.com>
2023-03-08 12:24:39 +0530

    Video 74 - ✔ Setting up a React Application for Redux - ✔

---
+++ src/components/Shop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Shop() {
+  return (
+    <div>
+      <h2 className="mb-3">But Adidas Shop 50 Rs</h2>
+      <button className="btn btn-primary"> - </button>
+      <span className="mx-2">Add to Card</span>
+      <button className="btn btn-primary"> + </button>
+    </div>
+  );
+}
+
+export default Shop;
